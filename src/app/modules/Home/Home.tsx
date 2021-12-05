af8c7214c1a2f78790f8f5ba355fed8f7fc157c4
tienphamvanvn <tienphamvanvn@gmail.com>
2021-12-05 08:39:27 +0700

    make topbar

--- src/app/modules/Home/Home.tsx
+++ src/app/modules/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style9 from "style9";
+import Topbar from "./Topbar";
 
 // styles
 import { base } from "@/app/styles/baseClasses";
@@ -10,6 +11,7 @@ import CommonLayout from "@/app/layouts/CommonLayout";
 const Home: React.FC = () => {
   return (
     <div className={style9(base.root)}>
+      <Topbar />
       <CommonLayout sidebar={<div />}>
         <div className={style9(base.root)}>Home Page</div>
       </CommonLayout>
